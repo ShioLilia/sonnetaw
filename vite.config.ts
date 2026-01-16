af669a54db04d8f62c92b9d89f381638451d10d1
Lilia <nixin985@gmail.com>
2026-01-16 10:58:11 +0900

    v0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
+  base: '/ShioLilia.github.io/src/sonnetaw/', // 改为你的仓库名称，或者如果部署到根目录就用 '/'
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
